5759d98bc48ed704fc5c12e253aa5c8a90fbc4c1
AjayPatil86 <ajay.patilmsc@gmail.com>
2023-06-17 18:33:44 +0530

    adding new project

---
+++ SampleDemo/SampleDemo/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleDemo
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            return "working";
+        }
+
+    }
+}
